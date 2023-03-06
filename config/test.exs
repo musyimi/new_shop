02027ea33effc712bb548e36a429586bc77fcb94
Musyimi <musyimi13@gmail.com>
2023-03-06 17:05:52 +0300

    first commit

---
+++ config/test.exs
@@ -0,0 +1,30 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :shop, Shop.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "shop_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :shop, ShopWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "p4Zz3uJD0C992jrXDg7m3I8/B4AY55XELB2fNZesRk9GKHcmSf7EWJ5shlIpHtTM",
+  server: false
+
+# In test we don't send emails.
+config :shop, Shop.Mailer, adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime

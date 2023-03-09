c02991f3683af9170a6e0d219b4edd91fd839045
Musyimi <musyimi13@gmail.com>
2023-03-09 11:08:20 +0300

    handling ecto changeset errors

---
+++ lib/shop_web/constants/constants.ex
@@ -0,0 +1,5 @@
+defmodule ShopWeb.Constants do
+  @internal_server_error "Internal Server Error"
+
+  def internal_server_error, do: @internal_server_error
+end
--- lib/shop_web/schema/resolvers/user_resolver.ex
+++ lib/shop_web/schema/resolvers/user_resolver.ex
@@ -1,13 +1,21 @@
 defmodule ShopWeb.Schema.Resolvers.UserResolver do
 
   alias Shop.Auth
+  alias ShopWeb.Utils
+  alias ShopWeb.Constants
 
   def register_user(_,%{input: input},_) do
+    case Auth.create_user(input) do
+      {:ok,_} ->
+         {:ok, true}
+      {:error, %Ecto.Changeset{} = changeset} ->
+      errors = Utils.format_changeset_errors(changeset)
+      {:error, errors}
 
-    IO.inspect(input)
-    res = Auth.create_user(input)
-    IO.puts("create_user => ")
-    IO.inspect(res)
-    {:ok, true}
+      {_,_} ->
+        {:error, Constants.internal_server_error()}
+
+
+    end
   end
 end
